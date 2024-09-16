709c2e6d50c9c2e055f438d6d6c25e600780879c
serious-cql <504458847@qq.com>
2024-09-16 23:39:54 +0800

    Site updated: 2024-09-16 23:39:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/20/hello-world/","2024/09/16/基于Typora的Markdown文档语法及操作/"];function toRandomPost(){
+var posts=["2024/07/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
