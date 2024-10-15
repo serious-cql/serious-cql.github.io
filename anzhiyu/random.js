a5ab483adbcfadd65a232a6a3139991ab5b8f5f2
serious-cql <504458847@qq.com>
2024-10-15 16:40:16 +0800

    Site updated: 2024-10-15 16:40:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/15/深度学习分类模型的训练与测试脚本/","2024/09/16/基于Typora的Markdown文档语法及操作/"];function toRandomPost(){
+var posts=["2024/09/16/基于Typora的Markdown文档语法及操作/","2024/10/15/深度学习分类模型的训练与测试脚本/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
